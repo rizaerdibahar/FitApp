5d04d4e9966c40ebba21c9ef9bbfbff9c88db248
İdil <idilblks@gmail.com>
2020-04-19 22:04:14 +0300

    start page added

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'change_password.dart';
 import 'package:flutter/material.dart';
 import 's_change_password.dart';
 import 's_change_email.dart';
-import 'back.dart';
+import 'start.dart';
 import 'package:ilkflutterapp/placeholder_widget.dart';
 
 void main() => runApp(App());
@@ -13,9 +13,9 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-    home: back(),
+    home: start(),
       routes: {
-        '/s_change_password': (context) => back(),
+        '/s_change_password': (context) => start(),
       },
     );
   }
